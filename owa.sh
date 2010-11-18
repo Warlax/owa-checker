58ccb5ccdd763f19fc99a4453663eb0a1f8c5ae5
niznash <niznash@21b5f407-ed80-ddb6-6d6f-6a525dc6b933>
2010-11-18 05:48:35 +0000

    added growlnotify support, added BSD license

--- owa.sh
+++ owa.sh
@@ -1,3 +1,31 @@
+#################################################################################
+# Copyright (c) 2010, Alejandro Nijamkin
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions are met:
+# 
+# Redistributions of source code must retain the above copyright notice, this 
+# list of conditions and the following disclaimer.
+# Redistributions in binary form must reproduce the above copyright notice, this 
+# list of conditions and the following disclaimer in the documentation and/or 
+# other materials provided with the distribution.
+# Neither the name Alejandro Nijamkin nor the names of its contributors may 
+# be used to endorse or promote products derived from this software without 
+# specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#################################################################################
+
 #!/bin/sh
 EXPECTED_ARGS=3
 
@@ -10,6 +38,9 @@ fi
 URL=$1
 USERNAME=$2
 PASSWORD=$3
+GROWL=$4
+
+echo $GROWL
 
 CURL=$(curl --cookie-jar cookies.txt --location --data "destination=$1&flags=4&forcedownlevel=0&trusted=4&username=$2&password=$3&isUtf8=1" $1/owa/auth/owaauth.dll)
 
@@ -18,6 +49,7 @@ count=0
 if [[ $CURL == *"not valid"* ]]
 then
 	count=-1
+	growlnotify --name "OWA Checker" --title "OWA Checker" --message "Invalid username and/or password!" --appIcon Mail.app
 else
 	if [[ $CURL == *">Inbox </a><span class=\"unrd\">"* ]]
 	then
@@ -26,7 +58,14 @@ else
 		then
 			CURL=$(echo $CURL | tr -d "^")
 			count=$CURL
+			emails="email."
+			if [ $count -gt 1 ]
+			then
+				emails="emails."
+			fi
+			growlnotify --name "OWA Checker" --title "OWA Checker" --message "You have ${count} unread ${emails}" --appIcon Mail.app
 		fi
     fi
 fi
+
 echo $count
