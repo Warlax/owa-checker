c9a264a1196299720fb663cf176be8e7858aaff5
niznash <niznash@21b5f407-ed80-ddb6-6d6f-6a525dc6b933>
2010-11-18 03:57:46 +0000

    Removed python code, renamed bashcript to curl.sh

--- trunk/owa.sh
+++ trunk/owa.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 EXPECTED_ARGS=3
-OWA_PY_PATH=/Users/Ale/Dropbox/Code/Personal/OwaNotifier/owa.py
-USER_DIR=/Users/Ale
 
-. $USER_DIR/.bash_profile
 if [ $# -ne $EXPECTED_ARGS ]
 then
   echo "Usage: URL USERNAME PASSWORD"
@@ -14,4 +11,22 @@ URL=$1
 USERNAME=$2
 PASSWORD=$3
 
-python $OWA_PY_PATH $URL $USERNAME $PASSWORD
+CURL=$(curl --cookie-jar cookies.txt --location --data "destination=$1&flags=4&forcedownlevel=0&trusted=4&username=$2&password=$3&isUtf8=1" $1/owa/auth/owaauth.dll)
+
+count=0
+
+if [[ $CURL == *"not valid"* ]]
+then
+	count=-1
+else
+	if [[ $CURL == *">Inbox </a><span class=\"unrd\">"* ]]
+	then
+		CURL=$(echo $CURL | sed 's/.*class="unrd">//' | sed 's/<.*//' | tr -d "\n" | tr "()" "^")
+		if [[ $CURL == *"^"* ]]
+		then
+			CURL=$(echo $CURL | tr -d "^")
+			count=$CURL
+		fi
+    fi
+fi
+echo $count
